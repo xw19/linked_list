d5d1ff333d1822528afcc2572882eef2ca6d9484
xw19 <sourav.moitr@gmail.com>
2015-02-14 20:45:53 +0530

    Improved according to reviews

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,73 +1,88 @@
-require_relative 'node'
+module LinkedList
 
-class LinkedList
-  def initialize(data) # constructor
-    if data.class == Array
-      @head = Node.new(data[0])
-      data.each.with_index { |datum, index| self.add(datum) if index > 0}
-    else
-      @head = Node.new(data[0])
+  class Node # this class defines one node the basic building blog of linked list
+    attr_accessor :data, :pointer # data for holding data and pointers refers to next node
+
+    def initialize(data, pointer = nil) #intializes when new is invoked
+      @data = data
+      @pointer = pointer
     end
-    self
-  end
 
-  def head #returns head
-    @head
-  end
-
-  def parse # parses all the members
-    node = @head
-    while (node != nil)
-      puts node.get_data
-      node = node.get_pointer
+    def to_s
+      puts "Data: #{data} Points to: #{self.pointer.data}"
     end
   end
 
-  def add(data)  # adds new members
-    node = @head
-    while (node.get_pointer != nil)
-      node = node.get_pointer
+  class SinglyLinkedList
+    def initialize(data) # constructor
+      if data.class == Array
+        @head = Node.new(data[0])
+        data.each.with_index { |datum, index| self.add(datum) if index > 0}
+      else
+        @head = Node.new(data[0])
+      end
+      self
     end
-    node.set_pointer(Node.new(data, nil))
-    self
-  end
 
-  def to_a # returns an array of all data
-    node = @head
-    array = Array.new
-    while (node != nil)
-      array << node.get_data
-      node = node.get_pointer
+    def head #returns head
+      @head
     end
-    array
-  end
 
-  def to_s # returns all data in form of string
-    node = @head
-    string = String.new
-    while (node != nil)
-      string << ", " << node.get_data.to_s
-      node = node.get_pointer
-    end
-    string
-  end
-
-  def delete(data) # deletes a single member
-    node = @head
-    deleted_node = ''
-    if node.get_data === data
-      @head = node.get_pointer
-      deleted_node = node
-    else
+    def parse # parses all the members
       node = @head
-      while( node != nil && node.get_pointer != nil && (node.get_pointer).get_data != data)
-        node = node.get_pointer
+      while (node != nil)
+        puts node.data
+        node = node.pointer
       end
-      if (node != nil) && (node.get_pointer != nil)
-        node.set_pointer((node.get_pointer).get_pointer)
-      end
-      deleted_node = node.get_pointer
     end
-    deleted_node = nil
+
+    def add(data)  # adds new members
+      node = @head
+      while (node.pointer != nil)
+        node = node.pointer
+      end
+      node.pointer = Node.new(data)
+      self
+    end
+
+    def to_a # returns an array of all data
+      node = @head
+      array = Array.new
+      while (node != nil)
+        array << node.data
+        node = node.pointer
+      end
+      array
+    end
+
+    def to_s # returns all data in form of string
+      node = @head
+      string = String.new(@head.data)
+      node = node.pointer
+      while (node != nil)
+        string << ", " << node.data.to_s
+        node = node.pointer
+      end
+      string
+    end
+
+    def delete(data) # deletes a single member
+      node = @head
+      deleted_node = ''
+      if node.data === data
+        @head = node.pointer
+        deleted_node = node
+      else
+        node = @head
+        while( node != nil && node.pointer != nil && (node.pointer).data != data)
+          node = node.pointer
+        end
+        if (node != nil) && (node.pointer != nil)
+          node.pointer = (node.pointer).pointer
+        end
+        deleted_node = node.pointer
+      end
+      deleted_node = nil
+    end
   end
 end
--- linked_list.gemspec
+++ linked_list.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'linked_list_sourav'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.date        = '2015-02-14'
   s.summary     = "Linked list library for ruby"
   s.description = "A simple linked list gem"
   s.authors     = ["Sourav Moitra"]
   s.email       = 'sourav.moitr@gmail.con'
   s.homepage    = 'https://github.com/xw19/linked_list'
-  s.files       = ["lib/linked_list.rb", "lib/node.rb"]
+  s.files       = ["lib/linked_list.rb"]
   s.license       = 'MIT'
 end
